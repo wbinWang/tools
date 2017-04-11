3b080f28a32b2af79d62542c5bdbea9cd7a653f8
王彬 <1462999792@qq.com>
2017-04-11 18:16:36 +0800

    测试
    测试一个正常.m.h文件

---
+++ wb_test/wbtest.h
@@ -0,0 +1,13 @@
+//
+//  wbtest.h
+//  Tools
+//
+//  Created by wenbin on 2017/4/11.
+//  Copyright © 2017年 beautyWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wbtest : NSObject
+
+@end
---
+++ wb_test/wbtest.m
@@ -0,0 +1,13 @@
+//
+//  wbtest.m
+//  Tools
+//
+//  Created by wenbin on 2017/4/11.
+//  Copyright © 2017年 beautyWang. All rights reserved.
+//
+
+#import "wbtest.h"
+
+@implementation wbtest
+
+@end
